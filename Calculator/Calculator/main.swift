9be0f3f50602493c1a25bd35edfdf7391a608c1c
Melinda Diaz <meld8bitgamer@Melindas-MacBook-Pro.local>
2019-10-18 15:35:32 -0400

    I tried  finish this with the limited amount of time I have I will get back to this:

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,23 +8,69 @@
 
 import Foundation
 
+var stop = false
+let operations = "+-*/"
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+    switch opString {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }
+    case "*":
+        return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
 }
 
+func secretGame() {
+    print("Secret Game Activated!! What is the missing operator, type \(operations)")
+    let operators = Int.random(in: 0..<4)
+    let number1 = Int.random(in: 0..<10)
+    let number2 = Int.random(in: 0..<10)
+    switch operators {
+    case 0: number1 + number2
+    case 1: number1 - number2
+    case 2: number1 * number2
+    case 3: number1 / number2
+    default:
+        print("Cannot compute try again"); secretGame()
+    }
+    
+}
 
-let closureOperation = mathStuffFactory(opString: "+")
+func basicCalc() {
+    let userInput:String = readLine() ?? ""
+    
+    let stringToArray = userInput.components(separatedBy: " ")
+    guard stringToArray.count == 3 && operations.contains(stringToArray[1]) else { if stringToArray.contains("?") { return secretGame()
+    }   else {
+        print("Unable to compute. Try again")
+        return basicCalc()
+        }
+    }
+    guard let indexZero = Double(stringToArray[0]) else {
+        print("Ask a valid question")
+        return basicCalc()
+    }
+    guard let indexTwo = Double(stringToArray[2]) else {
+        print("Ask a valid question")
+        return basicCalc()
+    }
+    
+    let closureOperation = mathStuffFactory(opString: stringToArray[1])
 
-let result = closureOperation(45, 5)
-print("result of operation is \(result)")
+    let result = closureOperation(indexZero,indexTwo)
+    
+    print("result of operation is \(result)")
+}
+
+print("enter your calculation question")
+
+repeat {
+    basicCalc()
+} while stop == false
+
